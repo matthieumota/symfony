226f23cb385c916d9d63461f4957ddb6b9776319
Matthieu Mota <matthieumota@gmail.com>
2026-03-16 15:58:23 +0100

    update 7.4

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -121,7 +121,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @see UserInterface
      */
-    public function eraseCredentials()
+    public function eraseCredentials(): void
     {
     }
 
